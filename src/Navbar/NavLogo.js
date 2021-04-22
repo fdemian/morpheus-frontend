e128954a2561da0f556a1bf82100ddb2ebbaa14f
fdcaminiti73 <federico.caminiti@leniolabs.com>
2021-04-22 20:42:04 -0300

    Fix manifest.json and Navbar logo.

--- src/Navbar/NavLogo.js
+++ src/Navbar/NavLogo.js
@@ -7,7 +7,7 @@ const NavLogo = ({mobile, blogName}) => (
   <Suspense fallback={<Spin />}>
      <Link to="/" className="topnav header-logo">
        <img
-          width={mobile ? 93 : 150}
+          width={mobile ? 93 : 96}
           height={mobile ? 30 : 50}
           src={logo}
           alt={blogName + " logo"}
