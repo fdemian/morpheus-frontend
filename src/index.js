3453c34d77a01ded68dc7735a85049655ee7e4c9
fdcaminiti73 <federico.caminiti@leniolabs.com>
2021-04-24 13:34:46 -0300

    Add react-snap.

--- src/index.js
+++ src/index.js
@@ -38,6 +38,12 @@ const Morpheus = () => {
   );
 };
 
-ReactDOM.render(<Morpheus />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(<Morpheus />, rootElement);
+} else {
+  ReactDOM.render(<Morpheus />, rootElement);
+}
 
 serviceWorker.unregister();
